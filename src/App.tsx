bf0c8d998c043370f52234eec7185cec639f7eec
jackkavanaugh55-lang <jackkavanaugh55@gmail.com>
2026-03-08 20:31:07 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Home from "./pages/home";
+import { Home } from "./pages/home";
 import Passport from "./pages/passport";
 
 function getPage() {
